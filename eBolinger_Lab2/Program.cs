dd8c8cdd35ba9bcfd00760e9b39846c362cacf3c
Easton Bolinger <bolinger@ksu.edu>
2020-09-02 17:23:51 -0500

    Add project files.

---
+++ eBolinger_Lab2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eBolinger_Lab2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
